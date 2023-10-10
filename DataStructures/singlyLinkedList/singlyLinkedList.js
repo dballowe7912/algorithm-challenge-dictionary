4286df0a5d4d80b3ad132698e74d4de1926495cc
Donald Ballowe <dballowe7912@gmail.com>
2023-10-09 19:39:09 -0500

    Added singlyLinkedList with push and pop methods

---
+++ DataStructures/singlyLinkedList/singlyLinkedList.js
@@ -0,0 +1,53 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+      var newNode = new Node(val)
+      if (!this.head) {
+        this.head = newNode;
+        this.tail = this.head;
+      } else {
+        this.tail.next = newNode;
+        this.tail = newNode;
+      }
+      this.length ++;
+      return this;
+    }
+    pop() {
+        if (!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while(current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+}
+
+var list = new SinglyLinkedList()
+list.push("HELLO")
+list.push("GOODBYE")
+list.push("Hello Hello")
+list.push("I Don't know why you say hello")
+list.push("I say goodbye")
+let peek = list.pop();
+console.log(list);
+console.log(peek.val);
---
+++ DataStructures/singlyLinkedList/singlyLinkedList.md
@@ -0,0 +1,18 @@
+### Singly Linked List
+A data structure that contains a head, tail and length property.
+Linked lists consists of nodes, and each node has a value and a pointer to another node.
+
+## Part 1: Push Method
+- should accept a value
+- creates a new node using the value
+- Edge case, if there is no head, set the node to be the head and tail
+- Otherwise set the next property on the tail to be the new node and set the tail property on the list to be the newly created node
+- increment length by one
+
+## Part 2: Pop Method
+- If there are no nodes in the list, return undefined
+- Loop through the list until you reach the tail
+- Set the next property of the 2nd to last node to null
+- Set the tail to be the 2nd to last node
+- Decrement the length of the list by 1
+- Return the value of removed node
